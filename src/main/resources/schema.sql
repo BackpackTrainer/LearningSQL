6726375a876b667b983740f063f9b5f79bac1bf4
BackpackTrainer <bill@fairfieldco.com>
2024-01-18 13:37:13 -0500

    Attempt to connect to MySQL database

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,13 +4,13 @@ CREATE TABLE COMPANY (
     company_name VARCHAR(250) NOT NULL
 );
 
-DROP TABLE IF EXISTS EMPLOYEES;
-CREATE TABLE EMPLOYEES (
+DROP TABLE IF EXISTS EMPLOYEE;
+CREATE TABLE EMPLOYEE (
     employee_id INT AUTO_INCREMENT  PRIMARY KEY,
     first_name VARCHAR(250) NOT NULL,
     last_name VARCHAR(250) NOT NULL,
     email VARCHAR(250) DEFAULT NULL,
     company_id INT,
-    foreign key (company_id) references COMPANY
+    foreign key (company_id) references COMPANY (company_id)
 );
 
