dcc39fc5e9adda15af54f9cb585bde1f0d85697f
BackpackTrainer <bill@fairfieldco.com>
2024-03-01 08:57:19 -0500

    Hopefully restoring a deleted repository and all its branches

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,14 @@
-DROP TABLE IF EXISTS COMPANY;
-CREATE TABLE COMPANY (
+CREATE TABLE IF NOT EXISTS COMPANY (
     company_id INT AUTO_INCREMENT  PRIMARY KEY,
     company_name VARCHAR(250) NOT NULL
 );
 
-DROP TABLE IF EXISTS EMPLOYEES;
-CREATE TABLE EMPLOYEES (
+CREATE TABLE IF NOT EXISTS EMPLOYEE (
     employee_id INT AUTO_INCREMENT  PRIMARY KEY,
     first_name VARCHAR(250) NOT NULL,
     last_name VARCHAR(250) NOT NULL,
     email VARCHAR(250) DEFAULT NULL,
     company_id INT,
-    foreign key (company_id) references COMPANY
+    foreign key (company_id) references COMPANY (company_id)
 );
 
